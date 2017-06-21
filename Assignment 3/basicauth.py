9d80dec16f11d98ea7445abc0c8d777630716fd8
royredman4 <royredman4@gmail.com>
2017-06-21 13:21:27 -0700

    Add files via upload

--- Assignment 3/basicauth.py
+++ Assignment 3/basicauth.py
@@ -1,43 +1,28 @@
 #!/usr/bin/env python
 
 from scapy.all import *
-import base64
 import socket
 
 
-def summarize(pkt):
-    print("New spot\n")
+def GetCurrentIP():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+
+def Vulnerability_Check(pkt):
     if pkt.haslayer(TCP):
         if (pkt.getlayer(Raw)):
             val = str(pkt[TCP].payload)
-            #pkt[TCP].show()
-            #temp = str(export_object(pkt.load))
-            #temp = temp.decode('base64')
-            #print(temp)
-            val = str(pkt[Raw].load)
-            print(val)
-            if (val.find("Authorization") != -1):
-                print("Found me a winner!!!" + val)
-            #val = (str(pkt[Raw].load))
-            #val = str(pkt[TCP].payload)
-            #print val
-            #val.decode('base64')
-            #print val.decode('base64')
-        
-    #print base64.b64decode(val)
-    #print val
-    #if (val.find("Authorization: Basic") != -1):
-    #    print "Found something!!\n" + val
-    #val = pkt.payload
-    #print val
-    #base64.b64decode(val)
-    #val.decode('base64')
-    #print str(val)
+            identifying_name = "Authorization: Basic"
+            index = val.find(identifying_name)
+            if (index != -1):
+                #print("Found me a winner!!!\n\n" + val)
+                user_info = val[index+len(identifying_name)+1: -1]
+                try:
+                    print("WARNING: Unprotected credentials! " + user_info.decode('base64'))
+                except:
+                    print("Error Decoding the following Authorization flaw " + user_info)
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-j = s.getsockname()[0]
-print "socketname is " + j
-sniff(prn=summarize, store=0, filter='host ' + j + ' and dst port 80')
-#sniff(prn=summarize, store=0)
+sniff(prn=Vulnerability_Check, store=0, filter='host ' + GetCurrentIP() + ' and dst port 80')
